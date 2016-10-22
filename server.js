0d29a2a008708eccab921d40a77e3dbda2d76609
Maxwell Renke <maxrenke@gmail.com>
2016-10-22 02:09:54 -0400

    added comments, client id and secret key, and link for login page

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http');
 var dispatcher = require('httpdispatcher');
 
+//localhost:3000
 const PORT = 3000;
+
+//Intralinks client api key
+var client_id = "B41PayHaro1unIcFzWXSoVU8r9KBD6z6";
+//Intralinks secret api key
+var secret =  "tkkkqxQvk1Z7OUwy";
+
 
 function handleRequest(request, response){
 	try{
@@ -20,14 +27,17 @@ server.listen(PORT, function(){
 
 dispatcher.setStatic('resources');
 
+//display home page
 function homePage(req,res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<html>\n<body>');
 	res.write('<h1>Intralinks Coding Exercise</h1>');
+	res.write('<a href=\"https://test-api.intralinks.com/v2/oauth/authorize?client_id=' + client_id + '&endOtherSessions=false\">Login</a>');
 	res.write('</body></html>');
 	res.end();
 }
 
+//at root, return home page
 dispatcher.onGet("/", function(req,res) {
 	homePage(req,res);
 });

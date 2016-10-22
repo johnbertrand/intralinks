eb63e3c3476938d99913db508a61f3d2a8598197
Maxwell Renke <maxrenke@gmail.com>
2016-10-22 02:18:03 -0400

    changed indentation to 2 spaces

--- server.js
+++ server.js
@@ -11,33 +11,33 @@ var secret =  "tkkkqxQvk1Z7OUwy";
 
 
 function handleRequest(request, response){
-	try{
-		console.log(request.url);
-		dispatcher.dispatch(request,response);
-	} catch(err) {
-		console.log(err);
-	}
+  try{
+console.log(request.url);
+    dispatcher.dispatch(request,response);
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 var server = http.createServer(handleRequest);
 
 server.listen(PORT, function(){
-	console.log("Server listening on localhost:%s", PORT);
+  console.log("Server listening on localhost:%s", PORT);
 });
 
 dispatcher.setStatic('resources');
 
 //display home page
 function homePage(req,res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('<html>\n<body>');
-	res.write('<h1>Intralinks Coding Exercise</h1>');
-	res.write('<a href=\"https://test-api.intralinks.com/v2/oauth/authorize?client_id=' + client_id + '&endOtherSessions=false\">Login</a>');
-	res.write('</body></html>');
-	res.end();
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<html>\n<body>');
+  res.write('<h1>Intralinks Coding Exercise</h1>');
+  res.write('<a href=\"https://test-api.intralinks.com/v2/oauth/authorize?client_id=' + client_id + '&endOtherSessions=false\">Login</a>');
+  res.write('</body></html>');
+  res.end();
 }
 
 //at root, return home page
 dispatcher.onGet("/", function(req,res) {
-	homePage(req,res);
+  homePage(req,res);
 });
